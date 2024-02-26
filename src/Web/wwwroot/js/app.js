bccb28b1d5761ae543ddcff7e5b6163f2441b641
Matt Lindsay <matthew.lindsay@acilearning.com>
2024-02-26 14:16:54 +0000

    Added dark mode theme persistence with JS cookies

--- src/Web/wwwroot/js/app.js
+++ src/Web/wwwroot/js/app.js
@@ -4,3 +4,47 @@
         ? window.navigator.brave?.isBrave()
         : false;
 };
+
+window.setCookie = (name, value, days) => {
+
+    if (!navigator.cookieEnabled) return;
+
+    if (days) {
+
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+
+    } else {
+        var expires = "";
+    }
+
+    document.cookie = name + "=" + value + expires + "; path=/";
+};
+
+window.getCookie = (name) => {
+
+    if (!navigator.cookieEnabled) return;
+
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+
+    for (var i = 0; i < ca.length; i++) {
+
+        var c = ca[i];
+
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1, c.length);
+        }
+
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+
+    return null;
+};
+
+window.deleteCookie = (name) => {
+    window.setCookie(name, "", -1);
+};
