a25c4ae9a4dd568c57e57ebb43d0cfb52b6a8887
Matt Lindsay <matthew.lindsay@acilearning.com>
2024-02-26 12:41:04 +0000

    Updated light and dark theme colors for better contrast

--- src/Web/Theming/DefaultTheme.cs
+++ src/Web/Theming/DefaultTheme.cs
@@ -9,6 +9,10 @@ public class DefaultTheme : MudTheme
 
         Palette = new PaletteLight()
         {
+            Background = "#f8f8f6",
+            Surface = "ffffff",
+            TextPrimary = "#262937",
+
             Primary = theme.PrimaryColor,
             PrimaryContrastText = theme.PrimaryAccentColor,
 
@@ -21,6 +25,10 @@ public class DefaultTheme : MudTheme
 
         PaletteDark = new PaletteDark()
         {
+            Background = "#262937",
+            Surface = "#2e313f",
+            TextPrimary = "#f8f8f6",
+
             Primary = theme.PrimaryColor,
             PrimaryContrastText = theme.PrimaryAccentColor,
 
@@ -41,6 +49,44 @@ public class DefaultTheme : MudTheme
             Default = new()
             {
                 FontFamily = theme.FontFamily,
+                FontSize = "1rem",
+                FontWeight = 400
+            },
+            H1 = new()
+            {
+                FontFamily = theme.FontFamily,
+                FontSize = "2.5rem",
+                FontWeight = 700
+            },
+            H2 = new() 
+            {
+                FontFamily = theme.FontFamily,
+                FontSize  = "2.25rem",
+                FontWeight = 600
+            },
+            H3 = new()
+            {
+                FontFamily = theme.FontFamily,
+                FontSize = "2rem",
+                FontWeight = 500
+            },
+            H4 = new()
+            {
+                FontFamily = theme.FontFamily,
+                FontSize = "1.75rem",
+                FontWeight = 500
+            },
+            H5 = new()
+            {
+                FontFamily = theme.FontFamily,
+                FontSize = "1.5rem",
+                FontWeight = 400
+            },
+            H6 = new()
+            {
+                FontFamily = theme.FontFamily,
+                FontSize = "1.25rem",
+                FontWeight = 300
             }
         };
     }
