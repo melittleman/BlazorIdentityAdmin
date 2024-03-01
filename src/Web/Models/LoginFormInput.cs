d187108d48c285cea6f36afe6a5e9bf22d3e8a3b
Matt Lindsay <matthew.lindsay@acilearning.com>
2024-03-01 12:20:32 +0000

    Fixed MudCheckbox default generic value being a string

--- src/Web/Models/LoginFormInput.cs
+++ src/Web/Models/LoginFormInput.cs
@@ -13,5 +13,12 @@ internal sealed record LoginFormInput
     public string Password { get; set; } = string.Empty;
 
     [Display(Name = "Remember me?")]
-    public bool RememberMe { get; set; } = false;
+    public string RememberMe { get; set; } = "off";
+
+    // This is a bit of a hack because currently the MudCheckbox
+    // component defaults to a string as it's generic T Value.
+    // We cannot change it because we're rendering in a Static SSR
+    // mode for the Login page, so hopefully this will be resolved
+    // once MudBlazor is fully compatible with .NET 8.
+    public bool IsRememberMeEnabled => RememberMe is "on";
 }
