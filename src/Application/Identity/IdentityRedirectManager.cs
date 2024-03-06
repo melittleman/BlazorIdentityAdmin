5666a8f0c6cc85b2db56dcd5eb3bdd097d78b055
Matt Lindsay <matthew.lindsay@acilearning.com>
2024-03-06 20:16:54 +0000

    Resolved issue with no AuthenticationState available after login

--- src/Application/Identity/IdentityRedirectManager.cs
+++ src/Application/Identity/IdentityRedirectManager.cs
@@ -13,7 +13,7 @@ public sealed class IdentityRedirectManager(NavigationManager navigationManager)
     };
 
     [DoesNotReturn]
-    public void RedirectTo(string? uri)
+    public void RedirectTo(string? uri, bool forceLoad = false)
     {
         uri ??= "";
 
@@ -25,31 +25,31 @@ public sealed class IdentityRedirectManager(NavigationManager navigationManager)
 
         // During static rendering, NavigateTo throws a NavigationException which is handled by the framework as a redirect.
         // So as long as this is called from a statically rendered Identity component, the InvalidOperationException is never thrown.
-        navigationManager.NavigateTo(uri);
+        navigationManager.NavigateTo(uri, forceLoad);
         throw new InvalidOperationException($"{nameof(IdentityRedirectManager)} can only be used during static rendering.");
     }
 
     [DoesNotReturn]
-    public void RedirectTo(string uri, Dictionary<string, object?> queryParameters)
+    public void RedirectTo(string uri, Dictionary<string, object?> queryParameters, bool forceLoad = false)
     {
         var uriWithoutQuery = navigationManager.ToAbsoluteUri(uri).GetLeftPart(UriPartial.Path);
         var newUri = navigationManager.GetUriWithQueryParameters(uriWithoutQuery, queryParameters);
-        RedirectTo(newUri);
+        RedirectTo(newUri, forceLoad);
     }
 
     [DoesNotReturn]
-    public void RedirectToWithStatus(string uri, string message, HttpContext context)
+    public void RedirectToWithStatus(string uri, string message, HttpContext context, bool forceLoad = false)
     {
         context.Response.Cookies.Append(StatusCookieName, message, StatusCookieBuilder.Build(context));
-        RedirectTo(uri);
+        RedirectTo(uri, forceLoad);
     }
 
     private string CurrentPath => navigationManager.ToAbsoluteUri(navigationManager.Uri).GetLeftPart(UriPartial.Path);
 
     [DoesNotReturn]
-    public void RedirectToCurrentPage() => RedirectTo(CurrentPath);
+    public void RedirectToCurrentPage(bool forceLoad = false) => RedirectTo(CurrentPath, forceLoad);
 
     [DoesNotReturn]
-    public void RedirectToCurrentPageWithStatus(string message, HttpContext context)
-        => RedirectToWithStatus(CurrentPath, message, context);
+    public void RedirectToCurrentPageWithStatus(string message, HttpContext context, bool forceLoad = false)
+        => RedirectToWithStatus(CurrentPath, message, context, forceLoad);
 }
--- src/Application/Identity/IdentityRevalidatingAuthenticationStateProvider.cs
+++ src/Application/Identity/IdentityRevalidatingAuthenticationStateProvider.cs
@@ -1,8 +1,8 @@
 namespace BlazorAdminDashboard.Application.Identity;
 
-// This is a server-side AuthenticationStateProvider that revalidates the security stamp for the connected user
-// every 30 minutes an interactive circuit is connected.
-internal sealed class IdentityRevalidatingAuthenticationStateProvider(
+// This is a server-side AuthenticationStateProvider that revalidates the security
+// stamp for the connected user every 30 minutes an interactive circuit is connected.
+public sealed class IdentityRevalidatingAuthenticationStateProvider(
         ILoggerFactory loggerFactory,
         IServiceScopeFactory scopeFactory,
         IOptions<IdentityOptions> options)
