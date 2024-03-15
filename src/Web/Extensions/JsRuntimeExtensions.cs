b4e772a25688535a21b65d2b029bdcc39f0d5524
Matt Lindsay <matthew.lindsay@acilearning.com>
2024-03-15 17:43:53 +0000

    Moved JS interop for form submission into extension method

---
+++ src/Web/Extensions/JsRuntimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorIdentityAdmin.Web.Extensions;
+
+internal static class JsRuntimeExtensions
+{
+    internal static async Task SubmitFormAsync(this IJSRuntime JS, string formId)
+    {
+        IJSObjectReference loginForm = await JS.InvokeAsync<IJSObjectReference>("document.getElementById", formId);
+        await loginForm.InvokeVoidAsync("submit");
+    }
+}
